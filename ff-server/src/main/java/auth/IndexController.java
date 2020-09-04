6215a080b021bfe707f227307cde9ca701a17f3d
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2020-09-04 12:54:19 +0700

    FF-2 Simple Rest Sever

---
+++ ff-server/src/main/java/auth/IndexController.java
@@ -0,0 +1,22 @@
+package auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "")
+public class IndexController {
+  private final String place;
+
+  public IndexController(String place) {
+    this.place = place;
+  }
+
+  @GetMapping(value = "/")
+  @ResponseBody
+  public String home() {
+    return String.format("Welcome %s", this.place);
+  }
+}
